0ce87396c60726824a2fb376b8b7ab10a8c83499
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-07-17 18:37:27 -0300

    feat
    Implemented validations with DTOs and Pipes.

---
+++ src/user/dto/CreateUser.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator"
+
+export class CreateUserDTO {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string
+
+    @IsEmail()
+    email: string
+
+    @MinLength(6)
+    password: string
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,13 @@
 import { Body, Controller, Get, Post } from "@nestjs/common"
+import { CreateUserDTO } from "./dto/CreateUser.dto"
 import { UserRepository } from "./user.repository"
 
 @Controller("/users")
 export class UserController {
-
-  constructor(
-    private userRepository: UserRepository
-  ) {}
+  constructor(private userRepository: UserRepository) {}
 
   @Post()
-  async create(@Body() data) {
+  async create(@Body() data: CreateUserDTO) {
     this.userRepository.save(data)
     return data
   }
