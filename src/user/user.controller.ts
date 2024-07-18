e57f4db5902de34f9c2dcd80f6af824d9aa145c2
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-07-18 12:44:24 -0300

    feat
    Created a DTO to validate data output in the API.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from "@nestjs/common"
 import { v4 as uuid } from "uuid"
 import { CreateUserDTO } from "./dto/CreateUser.dto"
+import { ListUserDTO } from "./dto/ListUser.dto"
 import { UserEntity } from "./user.entity"
 import { UserRepository } from "./user.repository"
 
@@ -16,11 +17,17 @@ export class UserController {
     userEntity.email = data.email
     userEntity.password = data.password
     this.userRepository.save(userEntity)
-    return { id: userEntity.id, message: "User created successfully!" }
+    return {
+      user: new ListUserDTO(userEntity.id, userEntity.name),
+      message: "User created successfully!"
+    }
   }
 
   @Get()
   async list() {
-    return this.userRepository.list()
+    const userData = await this.userRepository.list()
+    const userList = userData.map((user) => new ListUserDTO(user.id, user.name))
+
+    return userList
   }
 }
