44d0b643b2c420467692599e9d6f61f44ea8f73f
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-07-23 14:49:43 -0300

    feat
    Database connection configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from "@nestjs/common"
-import { UserModule } from "./user/user.module"
+import { ConfigModule } from "@nestjs/config"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { PostgresConfigService } from "config/db.config"
 import { ProductModule } from "./product/product.module"
+import { UserModule } from "./user/user.module"
 
 @Module({
   imports: [
     UserModule,
-    ProductModule
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
   ]
 })
 export class AppModule {}
